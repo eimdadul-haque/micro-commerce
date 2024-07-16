9824ba58384d2422a6ca9463d4aa8fef6baf7628
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-07-17 00:23:01 +0600

    Product and common entity added

---
+++ Catalog API/src/Catalog.Domain/Common/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Common
+{
+    public class Entity<T>
+    {
+        public T Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime modified { get; set; }
+        public string CreatorId { get; set; }
+        public string modifierId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Catalog API/src/Catalog.Domain/products/Product.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.products
+{
+    public class Product : Entity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string ImagePath { get; set; }
+        public decimal Price { get; set; }
+    }
+}
