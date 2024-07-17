0067e4276da63a3cc24b9cc8f5c91b2a5f6bd457
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-07-17 23:34:46 +0600

    product entity added in dbcontext

---
+++ Catalog API/src/Catalog.Infrastructure/CatalogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Domain.products;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
