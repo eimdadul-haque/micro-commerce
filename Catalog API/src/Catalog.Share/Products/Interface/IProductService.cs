8182899deaa509dd5e5030ed7feb2a73da8c7314
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-07-17 23:33:31 +0600

    product interface added

---
+++ Catalog API/src/Catalog.Share/Products/Interface/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Share.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Share.Products.Interface
+{
+    public interface IProductService
+    {
+        Task<IList<ProductDto>> Get(ProductFilterDto filter);
+        Task<ProductDto> Get(int id);
+        Task Add(ProductDto input);
+        Task Update(ProductDto input);
+        Task Delete(int id);
+    }
+}
