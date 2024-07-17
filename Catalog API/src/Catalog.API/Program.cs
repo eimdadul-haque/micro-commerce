c7c0b86aa3032a4dbcc108425b31fdf9ad930d32
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-07-18 00:13:35 +0600

    swagger added

--- Catalog API/src/Catalog.API/Program.cs
+++ Catalog API/src/Catalog.API/Program.cs
@@ -9,21 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<CatalogDbContext>((option) =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<CatalogDbContext>(option => option
+    .UseSqlServer(builder.Configuration
+    .GetConnectionString("DefaultConnection")));
 
 builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+});
 
 app.Run();
